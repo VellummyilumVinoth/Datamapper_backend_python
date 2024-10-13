4203324742d9f9bdac3ecb125bdcdac8fbce7071
Vellummyilum Vinoth <vellummyilumvinoth1999@gmail.com>
2024-10-13 20:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
+
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import base64
 import io
 import os
@@ -39,14 +55,14 @@ supported_content_types = [
 ]
 
 # Endpoint for generating mapping instructions
-@app.route("/generate_mapping_instruction/", methods=["POST"])
+@app.route("/file_upload/generate_mapping_instruction/", methods=["POST"])
 def generate_mapping_instruction():
     file = request.files.get('file')
     text = request.form.get('text')
     return process_input(file, text, process_type="mapping_instruction")
 
 # Endpoint for generating records
-@app.route("/generate_record/", methods=["POST"])
+@app.route("/file_upload/generate_record/", methods=["POST"])
 def generate_record():
     file = request.files.get('file')
     text = request.form.get('text')
